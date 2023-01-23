074ab92ac412c57993ae6caeb0f80b1e94cc0754
Katie Liu <katieliu59@gmail.com>
2023-01-23 18:33:59 -0500

    Implement basic HTML and EJS in urls_index to display a table of the urls and their shortened forms

--- express_server.js
+++ express_server.js
@@ -4,7 +4,7 @@ const PORT = 8080; // default port 8080
 app.set("view engine", "ejs");
 
 const urlDatabase = {
-	b2xVn2: "http://www.lighthouselabs.ca",
+	"b2xVn2": "http://www.lighthouselabs.ca",
 	"9sm5xK": "http://www.google.com",
 };
 
@@ -13,6 +13,10 @@ app.get("/", (req, res) => {
 });
 app.get("/hello", (req, res) => {
 	res.send("<html><body>Bonjour Hi World</body></html>");
+});
+app.get("/urls", (req, res) => {
+	const templateVars = { urls: urlDatabase };
+	res.render("urls_index", templateVars);
 });
 app.get("/urls.json", (req, res) => {
 	res.json(urlDatabase);
