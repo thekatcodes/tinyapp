48afcd724b3da7dcba74e2d5fbea8ff34e5e1d5e
Katie Liu <katieliu59@gmail.com>
2023-01-23 16:36:37 -0500

    Add new endpoint that contains html code which will be rendered in the client browser

--- express_server.js
+++ express_server.js
@@ -3,18 +3,19 @@ const app = express();
 const PORT = 8080; // default port 8080
 
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+	b2xVn2: "http://www.lighthouselabs.ca",
+	"9sm5xK": "http://www.google.com",
 };
 
 app.get("/", (req, res) => {
-  res.send("Hello!");
+	res.send("Hello!");
+});
+app.get("/hello", (req, res) => {
+	res.send("<html><body>Bonjour Hi World</body></html>");
 });
 app.get("/urls.json", (req, res) => {
-    res.json(urlDatabase);
-  });
+	res.json(urlDatabase);
+});
 app.listen(PORT, () => {
-    console.log(`Example app listening on port ${PORT}!`);
-    
-    
+	console.log(`Example app listening on port ${PORT}!`);
 });
