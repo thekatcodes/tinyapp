c7a94cb6a0bf508b328a86a1c788c3c296a1dbeb
Katie Liu <katieliu59@gmail.com>
2023-01-23 19:26:31 -0500

    Add a GET route to show the form

--- express_server.js
+++ express_server.js
@@ -8,6 +8,9 @@ const urlDatabase = {
 	"9sm5xK": "http://www.google.com",
 };
 
+app.get("/urls.json", (req, res) => {
+    res.json(urlDatabase);
+});
 app.get("/", (req, res) => {
 	res.send("Hello!");
 });
@@ -18,12 +21,12 @@ app.get("/urls", (req, res) => {
 	const templateVars = { urls: urlDatabase };
 	res.render("urls_index", templateVars);
 });
+app.get("/urls/new", (req, res) => {
+    res.render("urls_new");
+  });
 app.get("/urls/:id", (req, res) => {
     const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id]};
     res.render("urls_show", templateVars);
-  });
-app.get("/urls.json", (req, res) => {
-	res.json(urlDatabase);
 });
 app.listen(PORT, () => {
 	console.log(`Example app listening on port ${PORT}!`);
