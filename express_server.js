3dd4da3170003fad86c60c57876d743bda8dd5d6
Katie Liu <katieliu59@gmail.com>
2023-01-23 19:33:18 -0500

    Add a POST route to receive the froms submission and add body-parser to make data readable

--- express_server.js
+++ express_server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
 
 const urlDatabase = {
 	"b2xVn2": "http://www.lighthouselabs.ca",
@@ -21,6 +22,10 @@ app.get("/urls", (req, res) => {
 	const templateVars = { urls: urlDatabase };
 	res.render("urls_index", templateVars);
 });
+app.post("/urls", (req, res) => {
+    console.log(req.body); // Log the POST request body to the console
+    res.send("Ok"); // Respond with 'Ok' (we will replace this)
+  });
 app.get("/urls/new", (req, res) => {
     res.render("urls_new");
   });
