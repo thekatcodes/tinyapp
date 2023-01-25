80895464fe420ad6e4735b4ea8c78d93a0939c5d
Katie Liu <katieliu59@gmail.com>
2023-01-24 21:53:17 -0500

    Implement EJS logic to hide username form when username exists and display logout button, add POST route for /logout which clears the cookie and redirects the users to /urls page

--- express_server.js
+++ express_server.js
@@ -38,6 +38,10 @@ app.post("/login", (req, res) => {
 	res.cookie("username", req.body.username);
 	res.redirect("/urls");
 });
+app.post("/logout", (req, res) => {
+	res.clearCookie("username", req.body.username);
+	res.redirect("/urls");
+});
 app.get("/urls", (req, res) => {
 	const templateVars = { urls: urlDatabase, username: req.cookies["username"] };
 	res.render("urls_index", templateVars);
